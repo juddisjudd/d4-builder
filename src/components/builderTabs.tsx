0c29959e95dd86157633f582c87c6b7765c35360
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-09-09 11:34:51 -0700

    Remove showcase tab

--- src/components/builderTabs.tsx
+++ src/components/builderTabs.tsx
@@ -4,7 +4,6 @@ import { FC } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Hammer, Book, Diamond, NotebookText, Youtube, ChartBar, Shield } from 'lucide-react';
 import Notes from '@/app/builder/Notes';
-import Showcase from '@/app/builder/Showcase';
 import Aspects from '@/app/builder/gear/Aspects';
 import GemSection from '@/app/builder/gear/Gems';
 import SkillsAndMechanics from '@/app/builder/skills-and-mechanics';
@@ -41,10 +40,6 @@ const BuilderTabs: FC = () => {
           <NotebookText className="h-4 w-4" />
           <span>Notes</span>
         </TabsTrigger>
-        <TabsTrigger value="showcase" className="flex items-center space-x-1">
-          <Youtube className="h-4 w-4" />
-          <span>Showcase</span>
-        </TabsTrigger>
         <TabsTrigger value="stats" className="flex items-center space-x-1">
           <ChartBar className="h-4 w-4" />
           <span>Stats</span>
@@ -63,9 +58,6 @@ const BuilderTabs: FC = () => {
       </TabsContent>
       <TabsContent value="notes">
         <Notes />
-      </TabsContent>
-      <TabsContent value="showcase">
-        <Showcase />
       </TabsContent>
       <TabsContent value="stats">
         <div>Content specific to Stats for {selectedClass}</div>
